f87fca87dc3a3b61396d85dc5cea70d5ee92fd49
M-Semara <105911376+M-Semara@users.noreply.github.com>
2022-08-29 14:08:45 +0200

    push 1

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log("Yo les BG c'est michou");
+let form = document.getElementById("formulaire");
+let valeurInput = document.getElementById("valueInput");
+let ville = ""
+let meteo = []
+const API_KEY = "a0e300c65a961d94165e4f0a3b284336"
+
+
+
+const fetchMeteo = async () => {
+    const APIURI = `http://api.weatherstack.com/current?access_key=${API_KEY}&query=${ville}`;
+    meteo = await fetch (APIURI).then((Response) => Response.json())
+    console.log(meteo.current.temperature);
+} 
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    ville = valeurInput.value;
+    fetchMeteo()
+// Si la ville ne contient pas 3 lettre tu n'execute pas la fonction,
+
+if (ville.length > 2) {
+    fetchMeteo()
+    
+} else {
+   
+   result.innerHTML = 
+   ` <div class="alert alert-danger" role="alert">
+ Nom de la ville trop court!!!
+</div> `
+    
+}
+
+}
+) 
